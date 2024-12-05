fa587873ca3c734e08c702962543f63c6e079a24
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-05 13:22:07 -0500

    feat: custom security config

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,35 +2,34 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails John = User.builder()
-                .username("John")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails Mary = User.builder()
-                .username("Mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails Susan = User.builder()
-                .username("Susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
 
-        return new InMemoryUserDetailsManager(John, Mary, Susan);
+    // add support for JDBC authentication (users are stored in a database)
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve user information
+        jdbcUserDetailsManager
+                .setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve role information
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -53,5 +52,26 @@ public class DemoSecurityConfig {
         return http.build();
 
     }
+
+    // @Bean
+    // public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    // UserDetails John = User.builder()
+    // .username("John")
+    // .password("{noop}test123")
+    // .roles("EMPLOYEE")
+    // .build();
+    // UserDetails Mary = User.builder()
+    // .username("Mary")
+    // .password("{noop}test123")
+    // .roles("EMPLOYEE", "MANAGER")
+    // .build();
+    // UserDetails Susan = User.builder()
+    // .username("Susan")
+    // .password("{noop}test123")
+    // .roles("EMPLOYEE", "MANAGER", "ADMIN")
+    // .build();
+
+    // return new InMemoryUserDetailsManager(John, Mary, Susan);
+    // }
 
 }
